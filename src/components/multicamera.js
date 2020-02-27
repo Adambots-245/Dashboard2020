6116f4aa8dfbfc13b382ba2ea6da1ad657e665af
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-02-27 14:11:10 -0500

    Added smaller camera, and toggle button now sets the previous camera to the minicamera.

--- src/components/multicamera.js
+++ src/components/multicamera.js
@@ -1,6 +1,7 @@
 // Set a global alias for the camera and related elements.
 ui.camera = {
     viewer1: document.getElementById('camera'),
+    viewer2: document.getElementById("mini-camera"),
     button: "",
     id1: 0,
 	srcs: [ // Will default to first camera
@@ -8,12 +9,14 @@ ui.camera = {
         'http://roborio-245-frc.local:1181/?action=stream',
         'http://localhost:1183/?action=stream'
     ],
+    id2: 2,
     isStreaming: false
 };
 
 // Unlike most addons, this addon doesn't interact with NetworkTables. Therefore, we won't need to add any NT listeners.
 
 $(ui.camera.viewer1).html(`<div id="camera-bar"><p>Camera 1</p><button>Toggle</button></div>`);
+$(ui.camera.viewer2).html(`<div id="minicam-bar"><p>Camera 3</p></div>`);
 
 function checkIfStreaming(src, callback) {
     var elem = $(document.createElement("img")).attr("src", src).addClass("camera-preload");
@@ -40,10 +43,14 @@ let cameraListener = () => {
     ui.camera.button = document.querySelector("#camera button");
     ui.camera.button.onclick = function() {
         ui.camera.id1 += 1;
+        ui.camera.id2 += 1;
         if (ui.camera.id1 === ui.camera.srcs.length) ui.camera.id1 = 0;
+        if (ui.camera.id2 === ui.camera.srcs.length) ui.camera.id2 = 0;
         if (ui.camera.isStreaming) {
             ui.camera.viewer1.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id1] + ')';
+            ui.camera.viewer2.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id2] + ')';
             $(ui.camera.viewer1).html(`<div id="camera-bar"><p>Camera ${ui.camera.id1 + 1}</p><button>Toggle</button></div>`);
+            $(ui.camera.viewer2).html(`<div id="minicam-bar"><p>Camera ${ui.camera.id2 + 1}</p></div>`);
             ui.camera.button = document.querySelector("#camera button");
             cameraListener();
         }
@@ -52,12 +59,15 @@ let cameraListener = () => {
             checkIfStreaming(ui.camera.srcs[ui.camera.id1], (streaming) => {
                 if (streaming) {
                     ui.camera.viewer1.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id1] + ')';
+                    ui.camera.viewer2.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id2] + ')';
                     $(ui.camera.viewer1).html(`<div id="camera-bar"><p>Camera ${ui.camera.id1 + 1}</p><button>Toggle</button></div>`);
+                    $(ui.camera.viewer2).html(`<div id="minicam-bar"><p>Camera ${ui.camera.id2 + 1}</p></div>`);
                     ui.camera.isStreaming = true;
                 }
                 else {
                     ui.toast({text: `Camera ${ui.camera.id1 + 1} is not streaming`, duration: 5, type: "error"});
                     $(ui.camera.viewer1).html(`<div id="camera-bar"><p>Camera ${ui.camera.id1 + 1} <span class='error'>(Failed to Load)</span></p><button>Toggle</button></div>`);
+                    $(ui.camera.viewer2).html(`<div id="minicam-bar"><p>Camera ${ui.camera.id2 + 1} <span class='error'>(Failed to Load)</span></p></div>`);
                 }
                 ui.camera.button = document.querySelector("#camera button");
                 cameraListener();
@@ -72,11 +82,13 @@ let cameraListener = () => {
 checkIfStreaming(ui.camera.srcs[ui.camera.id1], (streaming) => {
     if (streaming) {
         ui.camera.viewer1.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id1] + ')';
+        ui.camera.viewer2.style.backgroundImage = 'url(' + ui.camera.srcs[ui.camera.id2] + ')';
         ui.camera.isStreaming = true;
     }
     else {
         ui.toast({text: `Camera ${ui.camera.id1 + 1} is not streaming`, duration: 5, type: "error"});
         $(ui.camera.viewer1).html(`<div id="camera-bar"><p>Camera 1 <span class='error'>(Failed to Load)</span></p><button>Toggle</button></div>`);
+        $(ui.camera.viewer2).html(`<div id="camera-bar"><p>Camera 3 <span class='error'>(Failed to Load)</span></p></div>`);
     }
     setTimeout(cameraListener, 1000);
 });
